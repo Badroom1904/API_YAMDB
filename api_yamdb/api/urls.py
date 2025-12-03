fd52980bf29feeea7258ddc21d6a1da848b92c5c
Andrey Zaitcev <3au4ukob@gmail.com>
2025-12-03 13:42:40 +0500

    Рабочий код с добавлением библиотеки

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,9 +2,13 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import AuthView, TokenView, UserViewSet
+from .views import CategoryViewSet, GenreViewSet, TitleViewSet
 
 router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet)
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('titles', TitleViewSet, basename='titles')
 
 extra_patterns = [
     path('', include(router_v1.urls)),
@@ -14,13 +18,4 @@ extra_patterns = [
 
 urlpatterns = [
     path('v1/', include(extra_patterns))
-from .views import CategoryViewSet, GenreViewSet, TitleViewSet
-
-router = DefaultRouter()
-router.register('categories', CategoryViewSet, basename='categories')
-router.register('genres', GenreViewSet, basename='genres')
-router.register('titles', TitleViewSet, basename='titles')
-
-urlpatterns = [
-    path('', include(router.urls)),
 ]
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'rest_framework',
     'rest_framework_simplejwt',
     'rest_framework',
     'django_filters',
