f32301986f472dcb4d9b3ac17d75ab5541b46881
Andrey Zaitcev <3au4ukob@gmail.com>
2025-12-04 20:48:40 +0500

    Форматировал код

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,4 @@
-from datetime import date
-
-from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 
 class Category(models.Model):
@@ -26,13 +21,14 @@ class Title(models.Model):
     name = models.CharField('Название', max_length=256)
     year = models.IntegerField('Год выпуска')
     description = models.TextField('Описание', blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, related_name='titles', verbose_name='Категория', blank=True, null=True)
-    genre = models.ManyToManyField(Genre, related_name='titles', verbose_name='Жанры', blank=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL,
+        related_name='titles', verbose_name='Категория', blank=True, null=True
+    )
+    genre = models.ManyToManyField(
+        Genre, related_name='titles',
+        verbose_name='Жанры', blank=True
+    )
 
     def __str__(self):
         return f'{self.name} ({self.year})'
-
-    def clean_year(self):
-        if self.year > date.today().year:
-            raise ValidationError('Нельзя добавлять произведения, которые еще не вышли.')
-        return self.year
